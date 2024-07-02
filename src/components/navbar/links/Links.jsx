b86136595adb4b064129a4452f20f5d48e2fb66a
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-07-02 10:48:38 +0700

    styling navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from "./links.module.css"
 
 const Links = () => {
     const links = [
@@ -20,7 +21,7 @@ const Links = () => {
         }
     ]
     return (
-        <div>
+        <div className={styles.links}>
             {
                 links.map(link => (
                     <Link href={link.path} key={link.title}>{link.title}</Link>
