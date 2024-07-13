ef95aea8350473cc2550aaf6bcfa29aeb5bb6e27
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-07-14 00:43:09 +0700

    feat: add active class to navlinks

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import styles from "./links.module.css"
+import NavLink from "./navLink/navLink";
 
 const Links = () => {
     const links = [
@@ -20,12 +20,31 @@ const Links = () => {
             path: "/blog"
         }
     ]
+
+    //TEMPORARY
+    const session = true
+    const isAdmin = true
+
     return (
         <div className={styles.links}>
             {
                 links.map(link => (
-                    <Link href={link.path} key={link.title}>{link.title}</Link>
+                    <NavLink item={link} key={link.title} />
                 ))
+            }
+            {
+                session ? (
+                    <>
+                        {
+                            isAdmin && (
+                                <NavLink item={{ title: "Admin", path: "/admin" }} />
+                            )
+                        }
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{ title: "Login", path: "/login" }} />
+                )
             }
         </div>
     )
