89e3da21fa2a2d8171b59efbd57b80f72e3574e7
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-07-22 23:47:10 +0700

    feat: add home and about page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Untuk menambahkan external link images di Next Image tag
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,26 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum 
+          voluptas cum repellendus sapiente, commodi saepe aut nostrum.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
   );
 }
