267db8d1a256bc9d68d077f5be9daf97ac6c8f67
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-07-28 22:48:45 +0700

    feat: data fetching on blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,22 +1,29 @@
 import PostCard from "@/components/postCard/PostCard";
 import styles from "./blog.module.css";
 
+const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-const BlogPage = () => {
+    if (!res.ok) {
+        throw new Error("Something went wrong!");
+    }
+
+    return res.json()
+}
+
+const BlogPage = async () => {
+
+    const posts = await getData();
+
     return (
         <div className={styles.container}>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
+            {
+                posts.map(post => (
+                    <div className={styles.post} key={post.id}>
+                        <PostCard post={post} />
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
     return (
         <div className={styles.container}>
             <div className={styles.top}>
@@ -12,11 +12,9 @@ const PostCard = () => {
                 <span className={styles.date}>01.01.2024</span>
             </div>
             <div className={styles.bottom}>
-                <h1 className={styles.title}>Title</h1>
-                <p className={styles.desc}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod dolore aut repudiandae culpa recusandae, ipsam vero corporis cupiditate aperiam ad impedit assumenda error doloremque suscipit sapiente ducimus qui molestias nulla.
-                </p>
-                <Link className={styles.link} href="/blog/post">READ MORE</Link>
+                <h1 className={styles.title}>{post.title}</h1>
+                <p className={styles.desc}>{post.body}</p>
+                <Link className={styles.link} href={`/blog/${post.id}`}>READ MORE</Link>
             </div>
         </div>
     )
